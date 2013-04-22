12619687b6a0ac3e3563a32cdbff87f120fbbebe
Gunnar A. Reinseth <gar@prepost.net>
2013-04-22 16:37:23 +0200

    Cleaned up the sources (in preparation to breaking them up in modules)

--- src/autoMock.js
+++ src/autoMock.js
@@ -1,95 +1,211 @@
-/* global jasmine, afterEach */
-define(function(require) {
+/*jshint camelcase:false*/
+(function(root) {
     'use strict';
 
-    var _ = require('underscore'),
-        factory = require('factory');
+    // TODO Lag egen Mock type som enkapsulerer jasmine.Spy
+
+    var require = root.require,
+        jasmine = root.jasmine;
+
+    var Objects = (function() {
+        return {
+            isString: function(obj) {
+                return Object.prototype.toString.call(obj) === '[object String]';
+            },
+            isFunction: function(obj) {
+                return Object.prototype.toString.call(obj) === '[object Function]';
+            },
+            isObject: function(obj) {
+                return obj === Object(obj);
+            },
+            isArray: function(obj) {
+                return Object.prototype.toString.call(obj) === '[object Array]';
+            },
+            clone: function(obj) {
+                if (!this.isObject(obj)) {
+                    return obj;
+                }
+                if (this.isArray(obj)) {
+                    return obj.slice();
+                }
+                var shallowCopy = {};
+                for (var prop in obj) {
+                    if (obj.hasOwnProperty(prop)) {
+                        shallowCopy[prop] = obj[prop];
+                    }
+                }
+                return shallowCopy;
+            }
+        };
+    })();
+
+    var Arrays = (function() {
+        return {
+            map: function(array, f) {
+                var mapped = [];
+                for (var i = 0; i < array.length; i++) {
+                    mapped[i] = f(array[i]);
+                }
+                return mapped;
+            }
+        };
+    })();
+
+    var Strings = (function() {
+        return {
+            endsWith: function(source, str) {
+                return source.substr(source.length - str.length) === str;
+            },
+            startsWith: function(source, str) {
+                return source.indexOf(str) === 0;
+            }
+        };
+    })();
+
+    var Modules = (function(root) {
+        var require = root.require;
+        var moduleInitializers = {};
+        var execCb = require.s.contexts._.execCb;
+
+        require.s.contexts._.execCb = function(name, moduleInitializer) {
+            moduleInitializers[name] = moduleInitializer;
+            return execCb.apply(this, arguments);
+        };
+
+        return {
+            has: function(name) {
+                return !!moduleInitializers[name];
+            },
+            init: function(name, customRequire) {
+                return moduleInitializers[name](customRequire || require);
+            }
+        };
+    })(root);
+
+    var ModuleNames = (function(Strings) {
+        var namePartRe = /^!?([^*]+)\*?$/;
+
+        function isWildcard(expression) {
+            return Strings.endsWith(expression, '*');
+        }
+
+        function isNegated(expression) {
+            return Strings.startsWith(expression, '!');
+        }
+
+        function extractNamePart(expression) {
+            return namePartRe.exec(expression)[1] || '';
+        }
+
+        function matches(expression, name) {
+            var namePart = extractNamePart(expression);
+            return namePart === name || (isWildcard(expression) && name.indexOf(namePart) === 0);
+        }
+
+        return {
+            isIncluded: function(expressionList, name) {
+                var included = false,
+                    expression;
+
+                for (var i = 0; i < expressionList.length; i++) {
+                    expression = expressionList[i];
+                    if (isNegated(expression) && matches(expression, name)) {
+                        included = false;
+                        break;
+                    }
+                    if (matches(expression, name)) {
+                        included = true;
+                    }
+                }
+
+                return included;
+            }
+        };
+    })(Strings);
+
+    function mock(obj, name) {
+        // TODO funksjoner med properties, og "klasser"
+        var result, value;
+        if (Objects.isFunction(obj)) {
+            result = jasmine.createSpy(name);
+        } else if (Objects.isObject(obj)) {
+            result = {};
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    value = obj[prop];
+                    if (Objects.isFunction(value)) {
+                        result[prop] = jasmine.createSpy(name + '.' + prop);
+                    } else {
+                        result[prop] = Objects.clone(value);
+                    }
+                }
+            }
+        } else {
+            result = Objects.clone(obj);
+        }
+        return result;
+    }
 
     /**
-     * @param {String} modulePath the module under test which will have its dependencies mocked
+     * @param {String} moduleName the module under test which will have its dependencies mocked
      * @param {Object} [options]
+     * @param {Object} [options.includes] a list of dependencies that should be included in the automock "circle"
      * @param {Object} [options.mocks] a map of mocks (path => instance) that autoMock should use (instead of creating
-     *                               automatically)
+     *                                 automatically)
      * @param {Array} [options.passthrough] a list of dependencies that should be passed through (i.e. not mocked)
      * @returns {*}
      */
-    window.autoMock = function(modulePath, options) {
+    function autoMock(moduleName, options) {
         options = options || {};
         options.mocks = options.mocks || {};
         options.passthrough = options.passthrough || [];
         options.includes = options.includes || [];
 
-        var moduleInitializer = factory.get(modulePath);
-        if (!moduleInitializer) {
-            throw 'Module "' + modulePath + '" has not yet been defined.';
+        if (!Modules.has(moduleName)) {
+            throw 'Module "' + moduleName + '" has not yet been defined.';
         }
 
         var dependencies = {};
 
-        function isIncluded(path) {
-            return _.any(options.includes, function(include) {
-                if (path === include) {
-                    return true;
-                }
-                if (include.substr(include.length - 1) === '*' && path.indexOf(include.substr(0, include.length - 1)) === 0) {
-                    return true;
-                }
-                return false;
-            });
-        }
-
         var requireFacade = jasmine.createSpy('require').andCallFake(function(deps, callback) {
             var result;
 
-            if (_.isString(deps)) {
+            if (Objects.isString(deps)) {
                 deps = [deps];
             }
 
-            result = _.map(deps, function(path) {
-                var realDependency,
-                    dependency;
+            result = Arrays.map(deps, function(dependencyName) {
+                var result;
 
-                if (dependencies[path]) {
-                    return dependencies[path];
+                if (dependencies[dependencyName]) {
+                    return dependencies[dependencyName];
                 }
 
-                if (options.mocks[path]) {
-                    dependency = options.mocks[path];
-                } else if (_.contains(options.passthrough, path)) {
-                    dependency = require(path);
-                } else if (isIncluded(path)) {
-                    dependency = factory.get(path)(requireFacade);
+                if (options.mocks[dependencyName]) {
+                    result = options.mocks[dependencyName];
+                } else if (ModuleNames.isIncluded(options.passthrough, dependencyName)) {
+                    result = require(dependencyName);
+                } else if (ModuleNames.isIncluded(options.includes, dependencyName)) {
+                    result = Modules.init(dependencyName, requireFacade);
                 } else {
-                    realDependency = require(path);
-                    if (_.isFunction(realDependency)) {
-                        dependency = jasmine.createSpy(path);
-                    } else if (_.isObject(realDependency)) {
-                        dependency = {};
-                        _.each(realDependency, function(value, key) {
-                            if (_.isFunction(value)) {
-                                dependency[key] = jasmine.createSpy(path + '.' + key);
-                            } else {
-                                dependency[key] = _.clone(value);
-                            }
-                        });
-                    } else {
-                        dependency = _.clone(realDependency);
-                    }
+                    result = mock(require(dependencyName), dependencyName);
                 }
 
-                dependencies[path] = dependency;
+                dependencies[dependencyName] = result;
 
-                return dependency;
+                return result;
             });
 
             if (callback) {
                 callback.apply(null, result);
+                return result;
             } else {
                 return result[0];
             }
         });
 
-        var module = moduleInitializer(requireFacade);
+        var module = Modules.init(moduleName, requireFacade);
+
         jasmine.getEnv().getMockFor = function(dependencyPath) {
             if (dependencyPath === 'require') {
                 return requireFacade;
@@ -100,17 +216,108 @@ define(function(require) {
             return dependencies[dependencyPath];
         };
         jasmine.getEnv().module = module;
-        jasmine.getEnv().modulePath = modulePath;
-
-        afterEach(function() {
-            delete jasmine.getEnv().getMockFor;
-            delete jasmine.getEnv().module;
-        });
+        jasmine.getEnv().modulePath = moduleName;
 
         return module;
-    };
+    }
 
-    window.getMockFor = function(dependencyPath) {
+    function getMockFor(dependencyPath) {
         return jasmine.getEnv().getMockFor(dependencyPath);
-    };
-});
+    }
+
+    function when(mock) {
+        if (mock === undefined) {
+            throw 'Illegal argument to "when": given function is undefined';
+        }
+        if (!jasmine.isSpy(mock)) {
+            throw 'Illegal argument to "when": given function is not a mock.';
+        }
+
+        return {
+            isCalledWith: function() {
+                var origReset = mock.reset;
+                if (!mock.whenMocks) {
+                    mock.whenMocks = [];
+                    mock.reset = function() {
+                        delete mock.whenMocks;
+                        origReset.call(this);
+                    };
+                }
+
+                var thenFunc = function() {
+                    var candidate,
+                        match,
+                        actualArgs = Array.prototype.slice.call(arguments);
+
+                    for (var i = mock.whenMocks.length; i > 0; i--) {
+                        candidate = mock.whenMocks[i - 1];
+                        if (jasmine.getEnv().equals_(actualArgs, candidate.args)) {
+                            match = candidate;
+                            break;
+                        }
+                    }
+
+                    if (match) {
+                        return match.func.apply(null, actualArgs);
+                    }
+                    return null;
+                };
+                mock.andCallFake(thenFunc);
+
+                return {
+                    _args: Array.prototype.slice.call(arguments),
+                    thenReturn: this.thenReturn,
+                    thenCall: this.thenCall
+                };
+            },
+            thenCall: function(func) {
+                if (mock.whenMocks) {
+                    mock.whenMocks.push({args: this._args, func: func});
+                } else {
+                    mock.andCallFake(func);
+                }
+            },
+            thenReturn: function(value) {
+                // TODO thenReturn og thenCall uten å gå via isCalledWith fungerer ikke hvis man har en miks av de
+                // Eksempel:
+                // when(mock).thenReturn(value); // default
+                // when(mock).isCalledWith(arg).thenReturn(otherValue)
+                // Dette fører til at første when aldri vil ta effekt dersom mocken blir kalt med noe annet enn 'arg'
+                this.thenCall(function() {
+                    return value;
+                });
+            }
+        };
+    }
+
+    jasmine.getEnv().beforeEach(function() {
+        this.addMatchers({
+            toHaveRequired: function(dep) {
+                var modulePath = this.env.modulePath;
+                var require = this.env.getMockFor('require');
+                this.message = function() {
+                    return [
+                        'Expected "' + modulePath + '" to have required ' + jasmine.pp(dep) + '.',
+                        'Expected "' + modulePath + '" to not have required ' + jasmine.pp(dep) + '.'
+                    ];
+                };
+                if (typeof dep === 'string') {
+                    return this.env.contains_(require.argsForCall, [dep]);
+                } else {
+                    return this.env.contains_(require.argsForCall, [dep, any(Function)]);
+                }
+            }
+        });
+    });
+
+    jasmine.getEnv().afterEach(function() {
+        delete jasmine.getEnv().getMockFor;
+        delete jasmine.getEnv().module;
+        delete jasmine.getEnv().modulePath;
+    });
+
+    root.getMockFor = getMockFor;
+    root.autoMock = autoMock;
+    root.when = when;
+
+})(this);
